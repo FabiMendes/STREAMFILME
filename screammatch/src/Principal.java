834bac3502b6f69a3f680ff54600c967e5c9c779
Fabiana Mendes <feelpitymendes@gmail.com>
2024-01-30 17:55:20 -0300

    Changes

--- screammatch/src/Principal.java
+++ screammatch/src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.alura.screammatch.modelos.Filme;
+import br.com.alura.screammatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -10,6 +11,7 @@ public class Principal {
         meuFilme.setNome("Coraline");
         meuFilme.setAnoDeLancamento(2007);
         meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
         //chamando alguns métodos do objeto meuFilme
         meuFilme.exibeFichaTecnica();
@@ -22,6 +24,14 @@ public class Principal {
         //System.out.println(meuFilme.totalDeAvaliacoes);
         System.out.println(meuFilme.obtemMedia());
 
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
 
     }
 }
---
+++ screammatch/src/br/com/alura/screammatch/modelos/Titulo.java
@@ -0,0 +1,63 @@
+package br.com.alura.screammatch.modelos;
+
+public class Titulo {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasavaliacaoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    //modificador de acesso
+    public int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    //Método para exibir a ficha técnica do filme
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do filme:" + nome);
+        System.out.println("Ano de lançamento:" + anoDeLancamento);
+    }
+    // // Método para avaliar o filme
+    public void avalia(double nota){
+        somaDasavaliacaoes += nota;
+        totalDeAvaliacoes ++;
+    }
+    //// Método para obter a média das avaliações
+    public double obtemMedia(){
+        return somaDasavaliacaoes / totalDeAvaliacoes;
+    }
+}
